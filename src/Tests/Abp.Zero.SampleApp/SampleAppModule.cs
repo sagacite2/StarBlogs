895c4cfea7b5a49556b81c68b1d78fc099fa6cde
Sagacite <sagacite@163.com>
2015-09-15 20:55:20 +0800

    Squashed 'ABP.Zero/' content from commit f0ef201
    git-subtree-dir: ABP.Zero
    git-subtree-split: f0ef201a22b8cfa0df5213c868a657725623d8de

---
+++ src/Tests/Abp.Zero.SampleApp/SampleAppModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Abp.Localization;
+using Abp.Modules;
+using Abp.Zero.EntityFramework;
+using Abp.Zero.SampleApp.Authorization;
+using Abp.Zero.SampleApp.Configuration;
+
+namespace Abp.Zero.SampleApp
+{
+    [DependsOn(typeof(AbpZeroEntityFrameworkModule))]
+    public class SampleAppModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.Localization.Languages.Add(new LanguageInfo("en", "English", isDefault: true));
+            Configuration.Localization.Languages.Add(new LanguageInfo("tr", "Türkçe"));
+
+            Configuration.Authorization.Providers.Add<AppAuthorizationProvider>();
+            Configuration.Settings.Providers.Add<AppSettingProvider>();
+            Configuration.MultiTenancy.IsEnabled = true;
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
