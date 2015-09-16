2a8378f1f818e1529eef3af9f5f33dac8d6a84ee
847039386 <ai847039386@vip.qq.com>
2015-09-16 12:09:42 +0800

    修改明星列表
    1、添加明星列表的分页功能

--- StarBlogs.Web/App/Main/views/manage/index.js
+++ StarBlogs.Web/App/Main/views/manage/index.js
@@ -2,7 +2,7 @@
     var controllerId = 'app.views.stars.index';
 
     angular.module('app')
-        .controller(controllerId, ['abp.services.app.star', '$scope', '$modal', function (starService, $scope, $modal) {
+        .controller(controllerId, ['abp.services.app.star', '$scope', '$modal', '$location', function (starService, $scope, $modal, $location) {
             var vm = this;
             vm.sortingDirections = ['Name Asc', 'CreationTime Desc', 'LastUpdateTime Desc'];
             vm.displaySortings = {
@@ -10,17 +10,27 @@
                 'CreationTime Desc': '按创建时间倒序',
                 'LastUpdateTime Desc': '按修改时间倒序'
             }
-            vm.stars = [];
+
+            //vm.stars = [];
             vm.sorting = 'CreationTime Desc';
             vm.starId = -1;
-            vm.loadStars = function (append) {
-                var skipCount = append ? vm.stars.length : 0;
+            vm.pageNumber = 3;          //每页数量
+            vm.pageBodyNumber = 3;
+            vm.maxCount = vm.totalStarCount != null ? vm.totalStarCount : 1;
+            vm.loadStars = function (append, page) {
+                page = page > 0 ? page : 0;
+                page = page > vm.maxCount - 1 ? vm.maxCount - 1 : page;
+                vm.currentPage = page > vm.maxCount - 1 ? vm.currentPage = vm.maxCount : vm.currentPage = page + 1;
+                $scope.sePage = page;
+                var skipCount = append ? vm.pageNumber * page : 0;
                 abp.ui.setBusy(
                     null,
                    starService.getStars({
+                       maxResultCount: vm.pageNumber,
                        skipCount: skipCount,
                        sorting: vm.sorting
                    }).success(function (data) {
+                       vm.stars = [];
                        if (append) {
                            for (var i = 0; i < data.items.length; i++) {
                                vm.stars.push(data.items[i]);
@@ -29,6 +39,46 @@
                            vm.stars = data.items;
                        }
                        vm.totalStarCount = data.totalCount;
+                       vm.pageNumberAll = [];
+                       vm.pageFos = page;
+                       vm.maxpageBodyNumber;
+                       vm.pageLength = vm.maxCount = Math.ceil(vm.totalStarCount / vm.pageNumber);
+
+
+                       if (page + vm.pageBodyNumber > vm.pageLength) {
+                           vm.pageFos = page - ((page + vm.pageBodyNumber) - vm.pageLength);
+                           vm.maxpageBodyNumber = vm.pageFos + vm.pageBodyNumber;
+                       }
+                       else {
+
+                           if (vm.pageFos < Math.ceil(vm.pageBodyNumber / 2) - 1) {
+
+
+                               vm.pageFos = (vm.pageFos + 1) - Math.ceil(vm.pageBodyNumber / 2);
+
+                               vm.pageFos = 0;
+
+
+                               vm.maxpageBodyNumber = vm.pageBodyNumber;
+
+                           } else {
+
+                               vm.pageFos = (vm.pageFos + 1) - Math.ceil(vm.pageBodyNumber / 2);
+
+                               vm.maxpageBodyNumber = vm.pageFos + vm.pageBodyNumber;
+                           }
+
+
+                       }
+
+                       for (var i = vm.pageFos; i < vm.maxpageBodyNumber; i++) {
+                           vm.pageNumberAll.push({ id: i + 1 });
+                       }
+
+
+
+
+
                    })
                 );
             };
@@ -39,7 +89,7 @@
                     vm.starTagSettings.forEach(function (item) {
                         vm['tagName' + item.id] = item.tagName;
                     });
-                    
+
                 });
             }
             vm.loadTagSettings();
@@ -138,7 +188,30 @@
             vm.showMore = function () {
                 vm.loadStars(true);
             };
+            vm.clickPaga = function (id) {
+                var pageId;
+                if (typeof id == 'boolean') {
+                    if (id) {
+                        pageId = $scope.sePage - 1;
 
-            vm.loadStars();
+
+                    } else {
+                        pageId = $scope.sePage + 1;
+                    }
+                } else {
+                    pageId = id - 1;
+                    vm.currentPage = pageId + 1;
+                }
+                vm.loadStars(true, pageId);
+
+
+
+
+            }
+
+            vm.clickPaga(0);
+
+
+
         }]);
 })();
